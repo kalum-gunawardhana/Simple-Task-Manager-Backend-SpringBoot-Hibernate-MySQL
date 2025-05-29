0c71a6109a5c56480fa6611f455f6244e39579e8
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-29 22:17:16 +0530

    commit

--- src/main/java/edu/TaskManager/model/Task.java
+++ src/main/java/edu/TaskManager/model/Task.java
@@ -1,5 +1,23 @@
 package edu.TaskManager.model;
 
-public class Task {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tasks")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed = false;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    // getters & setters
 }

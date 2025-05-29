79a4a4ebfa0f3ee69c2c5635232e093bf12ee423
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-29 22:21:23 +0530

    commit

--- src/main/java/edu/TaskManager/dto/TaskDto.java
+++ src/main/java/edu/TaskManager/dto/TaskDto.java
@@ -1,5 +1,17 @@
 package edu.TaskManager.dto;
 
-public class TaskDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed = false;
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
